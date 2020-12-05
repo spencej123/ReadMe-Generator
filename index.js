d9f62ee467f28e42c8552c28b17c837fe4e7689e
Spencer Johnson <spencer.johnson123@hotmail.com>
2020-12-04 20:30:09 -0600

    setting up my variables, working on functions

---
+++ index.js
@@ -0,0 +1,7 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+const promtUser = () => inquirer.prompt([]);
